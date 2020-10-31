b8efe09d3f66d397f439c3b24d4bb8c718b49cb3
haider <haider.sajjad@mail.utoronto.ca>
2020-10-31 01:41:35 -0400

    fixed a6 except for https

--- a2/softwareSecurity/index_fixed.php
+++ a2/softwareSecurity/index_fixed.php
@@ -4,21 +4,20 @@ session_start();
 ###################################################################################################
 # https://www.owasp.org/index.php/Top_10_2013-Top_10
 ###################################################################################################
-
 function sanitize_input($string, $isUserNameOrPassword)
 {
-    // maximum input length of 25 characters for username/password and 100 for expressions
+    // maximum input length of 50 characters for username/password and 100 for expressions
     if ($isUserNameOrPassword)
     {
-        $get_substring = substr($string, 0, 25);
-        $pattern = "/<|>|{|}|&|^|`|~|\/|\\\|\s/";
+        $get_substring = substr($string, 0, 50);
+        $pattern = "/<|>|{|}|&|\/|\\\|\\$/";
         $replacement = "";
         $cleaned_string = preg_replace($pattern, $replacement, $get_substring);
         return $cleaned_string;
     }
     else
     {
-        $get_substring = substr($string, 0, 100);
+        $get_substring = substr($string, 0, 200);
         $pattern = "/((sqrt)*[\^+*4\-()\/]+)+$/";
         return preg_match($pattern, $get_substring);
     }
@@ -52,7 +51,7 @@ $token = md5(uniqid(rand() , true));
 $_SESSION['token'] = $token;
 
 // make url to be used in the <form
-$url_with_token="http://192.168.10.100/fourFours/index.php?token=" . $token;
+$url_with_token = "http://192.168.10.100/fourFours/index.php?token=" . $token;
 
 if (!isset($_SESSION['isLoggedIn'])) $_SESSION['isLoggedIn'] = False;
 $operation = sanitize_input($_REQUEST['operation'], True);
@@ -75,133 +74,169 @@ function pg_connect_db()
 }
 
 // only continue on if tokens are the same
-if ($check_token) {
-  if ($operation == "login")
-  {
-      // sanitize inputs
-      $user = sanitize_input($_POST['user'], True);
-      $password = sanitize_input($_POST['password'], True);
-      $dbconn = pg_connect_db();
-      # FIX OWASP 2013 A1: SQL Injection, use prepared statements
-      $query = "SELECT id, username, firstName, lastName, passwd FROM account WHERE username=$1 AND passwd=$2";
-      $stmtname = "find_user";
-      $result = pg_prepare($dbconn, $stmtname, $query);
-      $result = pg_execute($dbconn, $stmtname, array(
-          $user,
-          $password
-      ));
-      if ($row = pg_fetch_row($result))
-      {
-
-          ###################################################################################################
-          # FIX OWASP 2013 A2: SESSION FIXATION
-          # Check that this application is vulnerable by bringing up firefox developer tools,
-          # visiting the website, then logging in, notice that the same cookie is used after
-          # change of privilege.
-          # Fix this by...
-          # Destroy current session and get a new session id when change in privilege.
-          # Check to make sure that the cookie changes when logged in
-          # Note: Some browsers now protect against reflection attacks, but not all.
-          # See: http://php.net/manual/en/function.session-regenerate-id.php
-          ###################################################################################################
-          $_SESSION['accountId'] = $row[0];
-          $_SESSION['user'] = $row[1];
-          $_SESSION['firstName'] = $row[2];
-          $_SESSION['lastName'] = $row[3];
-          $_SESSION['isLoggedIn'] = True;
-
-          session_regenerate_id(True);
-      }
-      else
-      {
-          $g_debug = "$user not logged in";
-          $_SESSION['isLoggedIn'] = False;
-      }
-  }
-  elseif ($operation == "deleteExpression")
-  {
-      $expressionId = sanitize_input($_REQUEST['expressionId'], True);
-      $accountId = sanitize_input($_REQUEST['accountId'], True);
-      $dbconn = pg_connect_db();
-
-      ###################################################################################################
-      # FIX OWASP 2013 A4: INSECURE DIRECT OBJECT REFERENCES
-      # Prove that it is vulnerable by logging in as one user and deleting another users entry
-      # Fix this by...
-      # Either fix the insecure part, that is, verify that the user can perform the operation
-      # of the direct object reference part, that is, fix the id's so they don't directly
-      # reference the expressionId, or both (even better).
-      # Another problem: why get account id from the request? In this case, this is part of the
-      # insecure direct object reference, that is, referencing the account id.
-      # Note: Simply not giving the user interface the option to delete is not sufficient.
-      ###################################################################################################
-      $stmtname = "delete_expression";
-      if ($accountId != $_SESSION['accountId']) {
-        $g_errors="ERROR: You cannot delete this Expression";
-      } else {
-        $result = pg_prepare($dbconn, $stmtname, "DELETE FROM solution WHERE id=$1 AND accountId=$2");
-        $result = pg_execute($dbconn, $stmtname, array(
-            $expressionId,
-            $accountId
-        ));
-      }
-  }
-  elseif ($operation == "addExpression")
-  {
-
-      ###################################################################################################
-      # FIX: XSS: user input/output is not vetted
-      # First check that the application is vulnerable by placing html in the
-      # database and then viewing the HTML as it exits the db
-      # Fix this by ...
-      # Either whitelisting the input, or escape the input
-      # Do the same for all untrusted input and output!
-      # http://stackoverflow.com/questions/46483/htmlentities-vs-htmlspecialchars
-      ###################################################################################################
-      $expression="";
-      $valid_expression=True;
-      if (sanitize_input($_POST['expression'], False)) {
-
-        // max length it can be is 100 so we don't mess up the ui for other users
-        $expression = substr($_POST['expression'], 0, 100);
-      } else {
-        $g_errors = "not a valid expression";
-        $valid_expression=False;
-      }
-
-      if ($valid_expression) {
-        $value = $_POST['value'];
-        $accountId = $_POST['accountId'];
-
+if ($check_token)
+{
+    if ($operation == "login")
+    {
+        // sanitize inputs
+        $user = sanitize_input($_POST['user'], True);
+        $password = sanitize_input($_POST['password'], True);
         $dbconn = pg_connect_db();
-        $stmtname = "find_expression";
-        $result = pg_prepare($dbconn, $stmtname, "SELECT * FROM solution WHERE expression=$1");
+
+        # A6 fix, using hashing sha256
+        // get salt from database
+        $query = "SELECT salt from account where username=$1";
+        $stmtname = "get_salt";
+        $result = pg_prepare($dbconn, $stmtname, $query);
         $result = pg_execute($dbconn, $stmtname, array(
-            $expression
+            $user
         ));
-        if (!($row = pg_fetch_row($result)))
+
+        // hash using hash( hash(salt) + password )
+        $username_found = False;
+        $hashed_password="";
+        if ($row = pg_fetch_row($result))
         {
-            $stmtname = "add_expression";
-            $result = pg_prepare($dbconn, $stmtname, "INSERT into solution (value, expression, accountId) values ($1, $2, $3)");
+            $salt = $row[0];
+            $username_found = True;
+            $hashed_password = hash("sha256", hash("sha256", $salt) . $password);
+        }
+
+        if ($username_found)
+        {
+            # FIX OWASP 2013 A1: SQL Injection, use prepared statements
+            $query = "SELECT id, username, firstName, lastName, passwd FROM account WHERE username=$1 AND passwd='$hashed_password'";
+            $stmtname = "find_user";
+            $result = pg_prepare($dbconn, $stmtname, $query);
             $result = pg_execute($dbconn, $stmtname, array(
-                $value,
-                $expression,
-                $accountId
+                $user
             ));
+            if ($row = pg_fetch_row($result))
+            {
+
+                ###################################################################################################
+                # FIX OWASP 2013 A2: SESSION FIXATION
+                # Check that this application is vulnerable by bringing up firefox developer tools,
+                # visiting the website, then logging in, notice that the same cookie is used after
+                # change of privilege.
+                # Fix this by...
+                # Destroy current session and get a new session id when change in privilege.
+                # Check to make sure that the cookie changes when logged in
+                # Note: Some browsers now protect against reflection attacks, but not all.
+                # See: http://php.net/manual/en/function.session-regenerate-id.php
+                ###################################################################################################
+                $_SESSION['accountId'] = $row[0];
+                $_SESSION['user'] = $row[1];
+                $_SESSION['firstName'] = $row[2];
+                $_SESSION['lastName'] = $row[3];
+                $_SESSION['isLoggedIn'] = True;
+
+                session_regenerate_id(True);
+            }
+            else
+            {
+                $g_debug = "$user not logged in";
+                $_SESSION['isLoggedIn'] = False;
+            }
+
         }
         else
         {
-            $g_errors = "$expression is already in our database";
+            $g_debug = "$user not logged in";
+            $_SESSION['isLoggedIn'] = False;
         }
-      }
+    }
+    elseif ($operation == "deleteExpression")
+    {
+        $expressionId = sanitize_input($_REQUEST['expressionId'], True);
+        $accountId = sanitize_input($_REQUEST['accountId'], True);
+        $dbconn = pg_connect_db();
 
-  }
-  elseif ($operation == "logout")
-  {
-      unset($_SESSION);
-      $_SESSION['isLoggedIn'] = False;
-      session_regenerate_id(True);
-  }
+        ###################################################################################################
+        # FIX OWASP 2013 A4: INSECURE DIRECT OBJECT REFERENCES
+        # Prove that it is vulnerable by logging in as one user and deleting another users entry
+        # Fix this by...
+        # Either fix the insecure part, that is, verify that the user can perform the operation
+        # of the direct object reference part, that is, fix the id's so they don't directly
+        # reference the expressionId, or both (even better).
+        # Another problem: why get account id from the request? In this case, this is part of the
+        # insecure direct object reference, that is, referencing the account id.
+        # Note: Simply not giving the user interface the option to delete is not sufficient.
+        ###################################################################################################
+        $stmtname = "delete_expression";
+        if ($accountId != $_SESSION['accountId'])
+        {
+            $g_errors = "ERROR: You cannot delete this Expression";
+        }
+        else
+        {
+            $result = pg_prepare($dbconn, $stmtname, "DELETE FROM solution WHERE id=$1 AND accountId=$2");
+            $result = pg_execute($dbconn, $stmtname, array(
+                $expressionId,
+                $accountId
+            ));
+        }
+    }
+    elseif ($operation == "addExpression")
+    {
+
+        ###################################################################################################
+        # FIX: XSS: user input/output is not vetted
+        # First check that the application is vulnerable by placing html in the
+        # database and then viewing the HTML as it exits the db
+        # Fix this by ...
+        # Either whitelisting the input, or escape the input
+        # Do the same for all untrusted input and output!
+        # http://stackoverflow.com/questions/46483/htmlentities-vs-htmlspecialchars
+        ###################################################################################################
+        $expression = "";
+        $valid_expression = True;
+        if (sanitize_input($_POST['expression'], False))
+        {
+
+            // max length it can be is 200 so we don't mess up the ui for other users
+            $expression = substr($_POST['expression'], 0, 200);
+        }
+        else
+        {
+            $g_errors = "not a valid expression";
+            $valid_expression = False;
+        }
+
+        if ($valid_expression)
+        {
+            $value = $_POST['value'];
+            $accountId = $_POST['accountId'];
+
+            $dbconn = pg_connect_db();
+            $stmtname = "find_expression";
+            $result = pg_prepare($dbconn, $stmtname, "SELECT * FROM solution WHERE expression=$1");
+            $result = pg_execute($dbconn, $stmtname, array(
+                $expression
+            ));
+            if (!($row = pg_fetch_row($result)))
+            {
+                $stmtname = "add_expression";
+                $result = pg_prepare($dbconn, $stmtname, "INSERT into solution (value, expression, accountId) values ($1, $2, $3)");
+                $result = pg_execute($dbconn, $stmtname, array(
+                    $value,
+                    $expression,
+                    $accountId
+                ));
+            }
+            else
+            {
+                $g_errors = "$expression is already in our database";
+            }
+        }
+
+    }
+    elseif ($operation == "logout")
+    {
+        unset($_SESSION);
+        $_SESSION['isLoggedIn'] = False;
+        session_regenerate_id(True);
+    }
 }
 
 $g_isLoggedIn = $_SESSION['isLoggedIn'];
--- fourFours/dev/schema.sql
+++ fourFours/dev/schema.sql
@@ -4,7 +4,8 @@ drop table solution cascade;
 create table account (
         id serial primary key,
         username varchar(50) UNIQUE,
-        passwd varchar(50) NOT NULL,
+        salt varchar(10) NOT NULL,
+        passwd varchar(64) NOT NULL,
         firstName varchar(50),
         lastName varchar(50)
 );
@@ -16,19 +17,18 @@ create table solution (
         accountId integer references account(id)
 );
 
-INSERT INTO account(firstName, lastName, username, passwd) values('Alex','Large','bigBoy','sdfdsfd');
-INSERT INTO account(firstName, lastName, username, passwd) values('Anne','Lion','anne','lion');
-INSERT INTO account(firstName, lastName, username, passwd) values('Linda','Swim','lindah20','fourfivesix');
-INSERT INTO account(firstName, lastName, username, passwd) values('Abagail','Silver','coins','silverisbetter');
-INSERT INTO account(firstName, lastName, username, passwd) values('Jessie','Burn','matchstick','password1');
-INSERT INTO account(firstName, lastName, username, passwd) values('Annie','Cup','coffee','password');
-INSERT INTO account(firstName, lastName, username, passwd) values('Diane','Bassell','ssll','passw0rd');
-INSERT INTO account(firstName, lastName, username, passwd) values('Steve','Mountain','cliff','cliff');
-INSERT INTO account (username, passwd, firstName, lastName) values ('arnold@cs.toronto.edu', 'sdxfdsgger', 'Arnold', 'Rosenbloom');
-INSERT INTO account(firstName, lastName, username, passwd) values('Jay','Perlmuter','perl','perl');
-INSERT INTO account(firstName, lastName, username, passwd) values('Peter','Piper','pickApeck','Peter');
-INSERT INTO account(firstName, lastName, username, passwd) values('Jen','Binghampton','hotel','jbh');
-INSERT INTO account(firstName, lastName, username, passwd) values('David','Kleinman','dk@gmail.com','esrever');
-INSERT INTO account(firstName, lastName, username, passwd) values('Jesse','Kowalski','eightball@gmail.com','badPassword');
-INSERT INTO account(firstName, lastName, username, passwd) values('Ivanna','Grant','ivanna','grant');
-
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Alex','Large','bigBoy', 'h6S6TV9ZQO','3b9c2126a5f3215ddfd3b161eab25a6123474e92551ae5fccfa2ee9204e90817');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Anne','Lion','anne','qGgNELUsuR','515e2fa5c2f72841e182512fb478ee1a8bda82101a581d6071f8c08bded556a9');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Linda','Swim','lindah20','TADfXcfLTA','6dbb7841871b7bde916d1345b51d97bdf4f49ed02f827577ba2e2139bfd26b7f');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Abagail','Silver','coins','jDpXcl0NcM','228c3d15af0dbb2a6d850e6517b3180c8ba52c5e3c7a21822a739e2ad3b7c903');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Jessie','Burn','matchstick','B9rPf8COA0','db536d4060562e628c2ab0998dd795a201fcbaa8c16d9b7ab841640a14e02726');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Annie','Cup','coffee','6XS8yYPz6j','cd5eb0a20b0af30b2f3740bce277467eb366218e95672327453ec0e65f4070b1');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Diane','Bassell','ssll','cfGy4hdDid','91ba019881f0c31cc456ced60ae4919e87b8919606914b992930aa82cd95a97b');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Steve','Mountain','cliff', 'L1mVlAMucY','01b1dec23632c9ea7bd9d45882be9fc49aec125bb8323f6b3220c94b4716805a');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Arnold', 'Rosenbloom','arnold@cs.toronto.edu', 'VrcSdKaWCl', '9db7541217ec6cc86bdd3ffa906b4cea3c709fdddd2ab0ab72c08227016aa0c2');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Jay','Perlmuter','perl', 'z5f1HfTBv1','22f2ec05c3bfed8212c1303df70d92bb96f0d14dc7f98c9e2bae4050fd575b0e');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Peter','Piper','pickApeck', 'mm3le524vf','303bb13d6fa2ca6fa217b7be6912edf41911905edc1316c2a439e69f22d7b401');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Jen','Binghampton','hotel', 'BKy8F6cqxe','14cfaf39b2defad4354b32520821b1685e2b194f88b922b628f1de8ebbbeca56');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('David','Kleinman','dk@gmail.com', 'rYQqsGjO0w','2056790772b710a894e3d396d71c270e240b3d47edc7e7039711db52c877e791');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Jesse','Kowalski','eightball@gmail.com', 'YGqbQB8VHs','d3058165c9186658cc8c99bb664bf0a749b3edd2499968bf456ba4ecefe122ff');
+INSERT INTO account(firstName, lastName, username, salt, passwd) values('Ivanna','Grant','ivanna', 'UTw035yFQL','d1cebe5afe1c24b3b1229ec4089f76755901250263d96554460cf58c9208fccd');
